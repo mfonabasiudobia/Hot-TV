ae6acda88cb49688deca1c948784454a3ef63328
MfonAbasi Udobia <mfonabasiisaac@fmail.com>
2023-08-18 16:57:45 +0100

    Fix admin route issue

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('jsdjsjjs', function(){
+    return 444;
+})->name('public.index');
+
+
 Route::group(['namespace' => "App\Http\Livewire"],function () {
 
 
@@ -18,6 +24,7 @@ Route::group(['namespace' => "App\Http\Livewire"],function () {
 
 
     });
+    
     
     
 });
