02925afde02345f8502bd3de7870b8d8a6326ef4
MfonAbasi Udobia <mfonabasiisaac@fmail.com>
2023-10-19 18:35:30 +0100

    Changed config/broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,9 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                // 'encrypted' => true,
+                'encrypted' => env('PUSHER_ENCRYPTED', true),
                 'host' => env('PUSHER_HOST', '127.0.0.1'),
-                'port' => 6001,
+                'port' => env('PUSHER_PORT', '6001'),
                 'scheme' => env('PUSHER_SCHEME', 'http')
             ],
         ],
