b06a127eb703acdf13a76e6eeaf13bc7fb2f177e
MfonAbasi Udobia <mfonabasiisaac@fmail.com>
2023-10-19 18:33:07 +0100

    Removed Broadcast from gitignore

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 // 'encrypted' => true,
-                'host' => '127.0.0.1',
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => env('PUSHER_SCHEME', 'http')
             ],
         ],
         'ably' => [
