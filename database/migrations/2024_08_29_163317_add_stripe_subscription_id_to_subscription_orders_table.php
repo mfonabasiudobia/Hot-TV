b74966b0872f637eaf35a99bab64e61a94983739
bahadur <bahadur.oad@gmail.com>
2024-08-30 03:40:18 +0500

    subscription module with trail based stripe

---
+++ database/migrations/2024_08_29_163317_add_stripe_subscription_id_to_subscription_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscription_orders', function (Blueprint $table) {
+            $table->string('stripe_subscription_id')->nullable()->after('subscription_id');
+            $table->timestamp('trial_ended_at')->nullable()->after('stripe_subscription_id');
+            $table->boolean('current_subscription')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscription_orders', function (Blueprint $table) {
+            $table->dropColumn('trial_ended_at');
+            $table->dropColumn('stripe_subscription_id');
+            $table->dropColumn('current_subscription');
+        });
+    }
+};
