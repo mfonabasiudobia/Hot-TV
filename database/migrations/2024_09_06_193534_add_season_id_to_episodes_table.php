109ce23a9591928f22ebf9417de853db0ac356c1
bahadur <bahadur.oad@gmail.com>
2024-09-11 18:36:35 +0500

    podcast resource updated

--- database/migrations/2024_09_06_193534_add_season_id_to_episodes_table.php
+++ database/migrations/2024_09_06_193534_add_season_id_to_episodes_table.php
@@ -26,7 +26,6 @@ return new class extends Migration
     public function down()
     {
         Schema::table('episodes', function (Blueprint $table) {
-            $table->dropIndex('season_id');
             $table->dropColumn('season_id');
         });
     }
