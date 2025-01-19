cbe7efbe1b476b4213b2a228903096ef3e1012b4
Naeem Akhtar <rana786naeem@gmail.com>
2025-01-19 13:02:27 +0500

    ongoing ride api updated

--- app/Http/Controllers/Api/V1/Customer/Ride/RideController.php
+++ app/Http/Controllers/Api/V1/Customer/Ride/RideController.php
@@ -2,11 +2,6 @@
 
 namespace App\Http\Controllers\Api\V1\Customer\Ride;
 
-use App\Enums\Api\V1\ApiResponseMessageEnum;
-use App\Events\DriverArrived;
-use App\Events\RideAccepted;
-use App\Events\RideSearching;
-use App\Events\RideStarted;
 use App\Http\Controllers\Controller;
 use App\Models\Ride;
 use Illuminate\Http\Request;
@@ -21,32 +16,14 @@ class RideController extends Controller
             ->first();
 
         if($ride) {
-            switch($ride->status) {
-                case 'requested':
-                    event(new RideSearching($ride, $ride->driver ?? null, $ride->customer));
-                    break;
-                case 'started':
-                    event(new RideStarted($ride, $ride->driver ?? null, $ride->customer));
-                    break;
-                case 'accepted':
-                    event(new RideAccepted($ride, $ride->driver ?? null, $ride->customer));
-                    break;
-                case 'driver-arrived':
-                    event(new DriverArrived($ride, $ride->driver ?? null, $ride->customer));
-                    break;
-                default:
-                    break;
-            }
-
+            $message = 'ride.' . $ride->status;
+            $data = ['id' => $ride->id];
         }
-
 
         return response()->json([
             'success' => true,
-            'message' => 'Inprogress ride',
-            'data' => [
-                'ride' => $ride,
-            ]
+            'message' => $message ?? 'no-ride',
+            'data' => $data ?? null
         ]);
     }
 }
